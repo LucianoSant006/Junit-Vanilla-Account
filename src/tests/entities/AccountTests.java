068ea0ff50eab3334ee954af6ca5e161ff7183fb
LucianoSant006 <santiagoluciano383@gmail.com>
2024-12-16 22:35:46 -0300

    depositShouldDoNothingWhenNegativeAmount

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -16,4 +16,13 @@ public class AccountTests {
 
         Assertions.assertEquals(expectedValue,acc.getBalance());
     }
+    @Test
+    public void depositShouldDoNothingWhenNegativeAmount(){
+        double expectedValue = 100.0;
+        Account acc = new Account(1l,expectedValue);
+        double amount = -200.0;
+        acc.deposit(amount);
+        Assertions.assertEquals(expectedValue,acc.getBalance());
+
+    }
 }

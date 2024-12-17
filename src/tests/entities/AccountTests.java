a4864d7d75bfecb3d90dac935425045bc1e96f7b
LucianoSant006 <santiagoluciano383@gmail.com>
2024-12-16 23:37:17 -0300

    implements FactoryAccount

--- src/tests/entities/AccountTests.java
+++ src/tests/entities/AccountTests.java
@@ -3,26 +3,28 @@ package tests.entities;
 import entities.Account;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import tests.factory.AccountFactory;
 
 public class AccountTests {
 
     @Test
-    public void depositShouldIncreaseBalanceWhenPositiveAmount(){
+    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
         double amount = 200.0;
         double expectedValue = 196.0;
-        Account acc = new Account(1l,0.0);
+        Account acc = AccountFactory.createEmptyAccount();
 
         acc.deposit(amount);
 
-        Assertions.assertEquals(expectedValue,acc.getBalance());
+        Assertions.assertEquals(expectedValue, acc.getBalance());
     }
+
     @Test
-    public void depositShouldDoNothingWhenNegativeAmount(){
+    public void depositShouldDoNothingWhenNegativeAmount() {
         double expectedValue = 100.0;
-        Account acc = new Account(1l,expectedValue);
+        Account acc = AccountFactory.createAccount(expectedValue);
         double amount = -200.0;
         acc.deposit(amount);
-        Assertions.assertEquals(expectedValue,acc.getBalance());
+        Assertions.assertEquals(expectedValue, acc.getBalance());
 
     }
 }
